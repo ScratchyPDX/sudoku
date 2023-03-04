62bcd889de792c3f2dc0b5978c8de576489c258f
David Ward <ward.david@live.com>
2023-03-03 18:39:44 -0800

    Add header and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
 import BoardClient from "./boardClient.js";
 const board = new BoardClient("#play-button", "#check-button", "#clear-button");
 board.init();
+
+document.getElementById("last_modified").innerHTML = `Last modified: ${document.lastModified}`;
