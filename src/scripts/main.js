7aec878c3c4752c15983d9283e021355cd27e338
David Ward <ward.david@live.com>
2023-03-07 19:06:23 -0800

    Remove last date

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,3 @@
 import BoardClient from "./boardClient.js";
 const board = new BoardClient("#play-button", "#check-button", "#reset-button", "#save-button");
 board.init();
-
-document.getElementById("last_modified").textContent = `Last modified: ${document.lastModified}`;
