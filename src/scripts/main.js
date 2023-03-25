4bec9bb04024690a6117d304789d987fa03b21a1
David Ward <David.Ward@standard.com>
2023-03-25 13:28:12 -0700

    Make app run full screen on mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 import BoardClient from "./boardClient.js";
+var elem = document.getElementById("body");
+if (elem.requestFullscreen) {
+  elem.requestFullscreen();
+} else if (elem.webkitRequestFullscreen) { /* Safari */
+  elem.webkitRequestFullscreen();
+} else if (elem.msRequestFullscreen) { /* IE11 */
+  elem.msRequestFullscreen();
+}
 const board = new BoardClient("#play-button", "#check-button", "#reset-button", "#save-button", "#load-button", "#delete-button");
 board.init();
