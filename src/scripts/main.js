19449bd1d88d2f221bbd33043d3c23830dd56415
David Ward <ward.david@live.com>
2023-03-26 18:41:33 -0700

    Another attempt a orientation lock

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 import BoardClient from "./boardClient.js";
-let screenOrientation = window.screen.orientation
-screenOrientation.lock("portrait-primary");
+let screenOrientation = window.screen.orientation;
+screenOrientation.lock("portrait");
 const board = new BoardClient("#play-button", "#check-button", "#reset-button", "#save-button", "#load-button", "#delete-button");
 board.init();
