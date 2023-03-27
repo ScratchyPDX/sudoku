eebeb13b289499d80a53ffc6c836d9b084aebff2
David Ward <ward.david@live.com>
2023-03-26 18:24:36 -0700

    Add screen orientation lock

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
 import BoardClient from "./boardClient.js";
+screen.orientation.lock("portrait");
 const board = new BoardClient("#play-button", "#check-button", "#reset-button", "#save-button", "#load-button", "#delete-button");
 board.init();
